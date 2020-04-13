cc6c6eb1544e3342d8ae136b7470177676abe6d4
taifi.guenre <guenretaifi@gmail.com>
2020-04-13 04:28:54 +0000

    follow, unfollow

--- app/Http/Controllers/UserFollowController.php
+++ app/Http/Controllers/UserFollowController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Micropost; // 追加
 
 class UserFollowController extends Controller
 {
@@ -16,5 +17,35 @@ class UserFollowController extends Controller
     {
         \Auth::user()->unfollow($id);
         return back();
+    }
+    
+    public function followings($id)
+    {
+        $user = User::find($id);
+        $followings = $user->followings()->paginate(10);
+
+        $data = [
+            'user' => $user,
+            'users' => $followings,
+        ];
+
+        $data += $this->counts($user);
+
+        return view('users.followings', $data);
+    }
+
+    public function followers($id)
+    {
+        $user = User::find($id);
+        $followers = $user->followers()->paginate(10);
+
+        $data = [
+            'user' => $user,
+            'users' => $followers,
+        ];
+
+        $data += $this->counts($user);
+
+        return view('users.followers', $data);
     }
 }
